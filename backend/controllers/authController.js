49161062f3f7313827f5e41e0fe905ca35599e1e
Brandon Lin <brandonlin9716@gmail.com>
2025-04-11 00:08:42 -0700

    Send token to user on authentication success

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,5 +1,8 @@
+import dotenv from 'dotenv';
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 import * as db from '../model/db.js';
+dotenv.config();
 
 const postLogin = async (req, res) => {
     const { username, password } = req.body;
@@ -11,7 +14,16 @@ const postLogin = async (req, res) => {
     if (!match) {
         res.status(401).json({ error: 'Password is incorrect.' });
     }
-    res.json({ message: 'success!' });
+    // Auth successful: sign token with user info
+    const options = {
+        expiresIn: 60
+    };
+    const token = jwt.sign(
+        { id: user.id, username: user.username, isAdmin: user.isAdmin },
+        process.env.TOKEN_SECRET,
+        options
+    );
+    res.json({ message: 'Authentication success!', token });
 };
 
 export { postLogin };
