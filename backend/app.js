bc04cb4c88899715ba9b8d45b92a6934b1a1634e
Brandon Lin <brandonlin9716@gmail.com>
2025-04-10 23:44:11 -0700

    Implement checking username and password on post /login

--- backend/app.js
+++ backend/app.js
@@ -9,6 +9,21 @@ app.use(express.urlencoded({ extended: true })); // enable data from post req
 
 // routes
 app.get('/', (req, res) => res.json('hey guys'));
+
+import bcrypt from 'bcryptjs';
+import * as db from './model/db.js';
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+    const user = await db.getUserByUsername(username);
+    if (!user) {
+        res.status(401).json({ error: 'Username is incorrect.' });
+    }
+    const match = await bcrypt.compare(password, user.password);
+    if (!match) {
+        res.status(401).json({ error: 'Password is incorrect.' });
+    }
+    res.json({ message: 'success!'});
+});
 app.use('/user', userRouter);
 
 // init server
