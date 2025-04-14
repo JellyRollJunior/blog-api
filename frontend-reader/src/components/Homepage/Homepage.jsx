e846bde26634a9ed84d94aca9967183838ebf3d8
Brandon Lin <brandonlin9716@gmail.com>
2025-04-14 15:41:48 -0700

    Display posts on homepage using data from /posts

--- frontend-reader/src/components/Homepage/Homepage.jsx
+++ frontend-reader/src/components/Homepage/Homepage.jsx
@@ -1,16 +1,33 @@
+import { useEffect, useState } from 'react';
+import { getRequest } from '../../api/api';
+
 const Homepage = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    getRequest('/posts')
+      .then((data) => setPosts(data));
+  }, []);
+
   return (
     <>
       <header>This is a header</header>
       <h1>Blog Name</h1>
       <main>
         <ul>
-          <li>Article <hr /></li>
-          <li>Article <hr /></li>
+          {posts.map((post) => {
+            return (
+              <li>
+                <h2>{post.title}</h2>
+                <p>{post.content}</p>
+                <hr />
+              </li>
+            );
+          })}
         </ul>
       </main>
     </>
   );
-}
+};
 
 export { Homepage };
