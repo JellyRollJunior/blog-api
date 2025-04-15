298bacbc70d609445b7fde5c760ba1466d2b6fef
Brandon Lin <brandonlin9716@gmail.com>
2025-04-14 21:36:34 -0700

    Center error text. Increase header font. Set rem to 16px

--- frontend-reader/src/components/Homepage/Homepage.jsx
+++ frontend-reader/src/components/Homepage/Homepage.jsx
@@ -29,7 +29,7 @@ const Homepage = () => {
           </ul>
         )}
         {posts && posts.length == 0 && <h2>No posts available</h2>}
-        {error && <h2>An error occurred. Please try again.</h2>}
+        {error && <h2 className={styles.error}>An error occurred. Please try again.</h2>}
       </main>
     </>
   );
