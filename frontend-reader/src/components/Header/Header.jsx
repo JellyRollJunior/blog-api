dd7125aee65d6cea0011539bd173cf625f722b27
Brandon Lin <brandonlin9716@gmail.com>
2025-04-16 22:31:07 -0700

    Place login msg on right. Extract nav positioning css from nav button

--- frontend-reader/src/components/Header/Header.jsx
+++ frontend-reader/src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ import shared from '../../styles/shared.module.css';
 const Header = ({ children }) => {
   return (
     <header className={styles.header}>
-      <nav>{children}</nav>
+      <nav className={styles.nav}>{children}</nav>
       <h1 className={`${shared.fontSizeHeadingLarge} ${shared.marginTopSmall}`}>The Chiikawa Chronicle</h1>
       <p className={shared.marginTopSmall}>なんか小さくてかわいいやつ</p>
       <hr className={shared.marginTopXLarge} />
