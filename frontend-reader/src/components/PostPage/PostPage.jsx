61b6d39aa1d899ab1b5cab6f615408b83f96d1c5
Brandon Lin <brandonlin9716@gmail.com>
2025-04-17 19:20:43 -0700

    Display post data

--- frontend-reader/src/components/PostPage/PostPage.jsx
+++ frontend-reader/src/components/PostPage/PostPage.jsx
@@ -1,9 +1,9 @@
 import { Link, useParams } from 'react-router-dom';
 import { Header } from '../Header/Header';
-import shared from '../../styles/shared.module.css';
 import { useEffect, useState } from 'react';
 import { getRequest } from '../../api/api';
 import { format } from 'date-fns';
+import shared from '../../styles/shared.module.css';
 
 const PostPage = () => {
   const postId = useParams().postId;
@@ -44,7 +44,18 @@ const PostPage = () => {
         </Link>
       </Header>
       <main>
-        <h1>hi guys its the post page!</h1>
+        <section className={shared.marginTopSmall}>
+          {post && (
+            <>
+              <h2 className={shared.fontSizeHeadingLarge}>{post.title}</h2>
+              <h4>
+                By {post.author.username} • {post.publishTime}
+              </h4>
+              <p className={shared.marginTopLarge}>{post.content}</p>
+            </>
+          )}
+        </section>
+        <section></section>
       </main>
     </>
   );
