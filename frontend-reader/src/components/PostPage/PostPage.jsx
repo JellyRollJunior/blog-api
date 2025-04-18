6e46893246150ce5fe2a2631966c866086b86d7b
Brandon Lin <brandonlin9716@gmail.com>
2025-04-17 20:27:33 -0700

    Display all post comments

--- frontend-reader/src/components/PostPage/PostPage.jsx
+++ frontend-reader/src/components/PostPage/PostPage.jsx
@@ -2,6 +2,7 @@ import { Link, useParams } from 'react-router-dom';
 import { Header } from '../Header/Header';
 import { usePost } from '../../hooks/usePost';
 import shared from '../../styles/shared.module.css';
+import styles from './PostPage.module.css';
 
 const PostPage = () => {
   const postId = useParams().postId;
@@ -22,14 +23,29 @@ const PostPage = () => {
           {post && (
             <>
               <h2 className={shared.fontSizeHeadingLarge}>{post.title}</h2>
-              <h4>
-                By {post.author.username} • {post.publishTime}
-              </h4>
+              <h4>By {post.author.username} • {post.publishTime}</h4>
               <p className={shared.marginTopLarge}>{post.content}</p>
             </>
           )}
         </section>
-        <section></section>
+        <section>
+          <h3 className={`${shared.marginTopXLarge} ${shared.fontSizeHeadingMedium}`}>Comments</h3>
+          <hr />
+          {post && post.comments.length > 0 && (
+            <ul className={shared.zeroPadding}>
+              {post.comments.map((comment) => (
+                <li className={`${shared.marginTopSmall} ${styles.comment}`} key={comment.id}>
+                  <h3 className={styles.commenter}>
+                    <span className={styles.username}>{comment.commenter.username}</span>
+                    <span className={styles.date}>  •  {comment.createdAt}</span>
+                  </h3>
+                  <p>{comment.content}</p>
+                </li>
+              ))}
+            </ul>
+          )}
+          {post && post.comments.length == 0 && <h1>No comments!</h1>}
+        </section>
       </main>
     </>
   );
