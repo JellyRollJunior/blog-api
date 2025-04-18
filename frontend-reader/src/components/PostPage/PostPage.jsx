b4fd80d9890c9156fb0848b66919af4cc9ce0f3d
Brandon Lin <brandonlin9716@gmail.com>
2025-04-17 18:56:16 -0700

    Fetch post data and store in post state

--- frontend-reader/src/components/PostPage/PostPage.jsx
+++ frontend-reader/src/components/PostPage/PostPage.jsx
@@ -1,8 +1,41 @@
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import { Header } from '../Header/Header';
 import shared from '../../styles/shared.module.css';
+import { useEffect, useState } from 'react';
+import { getRequest } from '../../api/api';
+import { format } from 'date-fns';
 
 const PostPage = () => {
+  const postId = useParams().postId;
+
+  const [post, setPost] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    const fetchPost = async () => {
+      setLoading(true);
+      try {
+        const post = await getRequest(`/posts/${postId}`, controller.signal);
+        console.log(post);
+        const date = new Date(post.publishTime);
+        post.publishTime = format(date, 'MMMM do, yyy');
+        setPost(post);
+        setError(null);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchPost();
+
+    return () => controller.abort();
+  }, [postId]);
+
   return (
     <>
       <Header>
