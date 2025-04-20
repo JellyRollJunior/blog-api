dac29758036004c8f41375e2d8a7235da19a4b1f
Brandon Lin <brandonlin9716@gmail.com>
2025-04-20 11:13:38 -0700

    Add icons to footer

--- frontend-reader/src/components/Footer/Footer.jsx
+++ frontend-reader/src/components/Footer/Footer.jsx
@@ -1,7 +1,15 @@
+import styles from './footer.module.css';
+
 const Footer = () => {
   return (
-    <footer>
-      <h1>This is a footer!</h1>
+    <footer className={styles.footer}>
+      <hr className={styles.footerSeparator}/>
+      <div className={styles.iconHolder}>
+        <a className={styles.github} href="https://github.com/jellyrolljunior" target="”_blank”"></a>
+        <a className={styles.linkedin} href="https://www.linkedin.com/in/jellyrolljunior/" target="”_blank”"></a>
+        <a className={styles.instagram} href="https://www.instagram.com/river.flows__" target="”_blank”"></a>
+      </div>
+      Created by JellyRollJunior (Brandon Lin)
     </footer>
   );
 };
